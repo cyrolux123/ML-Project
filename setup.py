d1a0717b151c033274cb2911c279b498b37479bd
Nabin Katwal <78csi26@texasintl.edu.np>
2024-10-23 08:01:16 +0545

    commit

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import find_packages, setup
+from typing import List
+
+E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of required packages
+    '''
+    requirements = []
+    with open(file_path, 'r') as file:
+        requirements = file.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+
+    return requirements
+
+
 
 setup(
     name = 'mlproject',
@@ -6,5 +24,5 @@ setup(
     author = 'Nabin',
     author_email = 'nabincoder@gmail.com',
     packages = find_packages(),
-    install_requires = ['pandas', 'numpy', 'seaborn']
+    install_requires = get_requirements('requirements.txt')
 )
