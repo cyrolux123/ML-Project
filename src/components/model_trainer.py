3065e271a2b94236164dbdf4dcd996122123b854
Nabin Katwal <78csi26@texasintl.edu.np>
2024-10-24 18:45:48 +0545

    nk

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -17,7 +17,7 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object
+from src.utils import save_object, evaluate_model
 
 @dataclass
 class ModelTrainerConfig:
@@ -48,3 +48,20 @@ class ModelTrainer:
             }
 
             model_report : dict = evaluate_model(X_train = X_train, y_train = y_train, X_test = X_test, y_test = y_test models = models)
+
+            # To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            # To get best model name from dict
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException('Model score is less than 0.6 so no best model found')
+            logging.info(f'Best model is {best_model_name} with score {best_model_score}')
+
+            save_object(
+                file_path = self.model_trainer_config.trained_model_file_path,
+                obj = best_model
+            )
